fc911a294bf261500e39b85b2a08e6c1632ec070
Isaiah <ijandicos90@gmail.com>
2022-08-22 14:19:04 -0400

    Completed Assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,15 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def create
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  end
+
+private
+
+def movie_params
+  params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+end
+
 end
